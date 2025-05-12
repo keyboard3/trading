90c729a3d9d8f7242d460406c9346e6400180d23
李聪 <codeaword@163.com>
2025-05-12 22:19:05 +0800

    update proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server: { 
     proxy: {
       '/api': {
-        target: 'http://localhost:8089', 
+        target: 'http://127.0.0.1:8089', 
         changeOrigin: true, 
       }
     }
